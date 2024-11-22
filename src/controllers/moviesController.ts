dc82940dd77f8e47614753150215b7311c9a4106
matthias <matthias.faucon@orange.fr>
2024-11-22 15:56:56 +0100

    feat: add search route

--- src/controllers/moviesController.ts
+++ src/controllers/moviesController.ts
@@ -3,11 +3,40 @@ import { AppDataSource } from '../data-source';
 import { In } from 'typeorm';
 import { Category } from '../entity/Categories';
 import { Movie } from '../entity/Movie';
+import { Like } from 'typeorm';
 
 // Get tout les films
 export async function getMovies(request: FastifyRequest, reply: FastifyReply) {
 	const movies = await AppDataSource.getRepository('Movie').find({ relations: ['categories'] });
 	return reply.send(movies);
+}
+
+// Recherche de films par titre
+export async function searchMovie(request: FastifyRequest, reply: FastifyReply) {
+	try {
+		const { title } = request.query as { title: string };
+		
+		if (!title) {
+			return reply.status(400).send({ error: 'Title query parameter is required.' });
+		}
+
+		// https://github.com/typeorm/typeorm/blob/master/docs/find-options.md#advanced-options
+		const movies = await AppDataSource.getRepository(Movie).find({ 
+			where: { 
+				original_name: Like(`%${title}%`)
+			},
+			relations: ['categories']
+		});
+
+		if (movies.length === 0) {
+			return reply.status(404).send({ message: 'No movies found with that title.' });
+		}
+
+		return reply.send(movies);
+	} catch (error) {
+		console.error('Error searching for movie:', error);
+		return reply.status(500).send({ error: 'Internal Server Error', details: (error as Error).message });
+	}
 }
 
 // Get un film par son ID
